0ca5ed99f7d099843dca1e6717a288897d93c0a9
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-04-19 02:21:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Ramzy';
+
+const welcomeMessage = 'Welcome to Flatbook, Ramzy!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, RAMZY!';
+
+const shortGreeting = 'Welcome, R!';
